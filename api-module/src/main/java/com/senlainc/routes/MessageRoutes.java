f24f1d26a7926ecbff0b7d2e3558135cac0a9e32
amazingred2020 <zero_limit@mail.ru>
2021-08-23 17:24:11 +0300

    refactor5

--- api-module/src/main/java/com/senlainc/routes/MessageRoutes.java
+++ api-module/src/main/java/com/senlainc/routes/MessageRoutes.java
@@ -2,7 +2,7 @@ package com.senlainc.routes;
 
 public interface MessageRoutes {
 
-    String MESSAGE = "/com.senlainc.unit.message";
+    String MESSAGE = "/message";
     String MESSAGE_BY_PARAMS = MESSAGE + "/find";
     String PAGINATION = MESSAGE + "/{page}/{size}";
 }
